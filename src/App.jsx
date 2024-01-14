97e4c917a5c1010d640f39767d338b63e4eb6a2e
hananhi <hananhimoni@gmail.com>
2024-01-14 11:53:01 +0200

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 function App() {
+
+  function func(){
+    console.log('hanan')
+  }
   return (
     <>
-      <Routes>{/* <Route  /> */}</Routes>
+
+    
+      <Routes>{/* <Route  /> */}
+      </Routes>
     </>
   );
 }
